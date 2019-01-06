6eb26663d0b0d41b6008ce536994a11aa389be80
Robin <sysatom@gmail.com>
2019-01-06 15:44:25 +0800

    update newsync

--- app/schedule/news_sync.js
+++ app/schedule/news_sync.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const moment = require('moment');
 const Subscription = require('egg').Subscription;
-const REPOS_URL_REGEX = /https?:\/\/github\.com\/([0-9a-zA-Z\-\.]+)\/([0-9a-zA-Z\-\.]+)/i;
+const moment = require('moment');
+const constant = require('../constant');
 
-class UpdateCache extends Subscription {
+class NewsSync extends Subscription {
 
   static get schedule() {
     return {
@@ -21,68 +21,75 @@ class UpdateCache extends Subscription {
     };
 
     for (const endpointsKey in endpoints) {
-      console.log(base + endpoints[endpointsKey]);
-      const res = await this.ctx.curl(base + endpoints[endpointsKey], {
-        timeout: 60000,
-      });
-      const items = res.data;
-      items.forEach(async id => {
-        const res = await this.ctx.curl(`${base}/v0/item/${id}.json'`, {
+      let items;
+      try {
+        const res = await this.ctx.curl(base + endpoints[endpointsKey], {
           timeout: 60000,
         });
-        const itemData = res.data;
-        let found;
-        if (typeof itemData.url === 'string') {
-          found = itemData.url.match(REPOS_URL_REGEX);
-        } else {
-          found = null;
-        }
-
-        if (found) {
-          const reposNews = await this.ctx.model.ReposNews.findOne({
-            where: {
-              url: itemData.url,
-              item_id: itemData.id,
-            },
+        items = res.data;
+      } catch (e) {
+        items = [];
+      }
+      items.forEach(async id => {
+        try {
+          const res = await this.ctx.curl(`${base}/v0/item/${id}.json'`, {
+            timeout: 60000,
           });
-          if (reposNews) {
-            const repos = await this.ctx.model.Repos.findOne({
-              where: {
-                slug: `${found[1]}-${found[2]}`,
-              },
-            });
-            if (repos) {
-              reposNews.repos_id = repos.id;
-            }
-            reposNews.score = itemData.score;
-            reposNews.save();
+          const itemData = res.data;
+          let found;
+          if (typeof itemData.url === 'string') {
+            found = itemData.url.match(constant.REPOS_URL_REGEX);
           } else {
-            const repos = await this.ctx.model.Repos.findOne({
+            found = null;
+          }
+          if (found) {
+            const reposNews = await this.ctx.model.ReposNews.findOne({
               where: {
-                slug: `${found[1]}-${found[2]}`,
+                url: itemData.url,
+                item_id: itemData.id,
               },
             });
-            let reposId = 0;
-            if (repos) {
-              reposId = repos.id;
+            if (reposNews) {
+              const repos = await this.ctx.model.Repos.findOne({
+                where: {
+                  slug: `${found[1]}-${found[2]}`,
+                },
+              });
+              if (repos) {
+                reposNews.repos_id = repos.id;
+              }
+              reposNews.score = itemData.score;
+              reposNews.save();
             } else {
-              const githubUrl = `https://github.com/${found[1]}/${found[2]}`;
-              this.ctx.app.queue.add({ job: 'fetchRepos', data: { url: githubUrl } });
+              const repos = await this.ctx.model.Repos.findOne({
+                where: {
+                  slug: `${found[1]}-${found[2]}`,
+                },
+              });
+              let reposId = 0;
+              if (repos) {
+                reposId = repos.id;
+              } else {
+                const githubUrl = `https://github.com/${found[1]}/${found[2]}`;
+                this.ctx.app.queue.add({ job: 'fetchRepos', data: { url: githubUrl } });
+              }
+              this.ctx.model.ReposNews.create({
+                url: itemData.url,
+                time: moment(itemData.time).toDate(),
+                repos_id: reposId,
+                title: itemData.title.replace('Show HN: ', ''),
+                score: itemData.score,
+                item_id: itemData.id,
+                post_date: moment(itemData).format('YYYY-MM-DD'),
+              });
             }
-            this.ctx.model.ReposNews.create({
-              url: itemData.url,
-              time: moment(itemData.time).toDate(),
-              repos_id: reposId,
-              title: itemData.title.replace('Show HN: ', ''),
-              score: itemData.score,
-              item_id: itemData.id,
-              post_date: moment(itemData).format('YYYY-MM-DD'),
-            });
           }
+        } catch (e) {
+          this.app.logger.error(e);
         }
       });
     }
   }
 }
 
-module.exports = UpdateCache;
+module.exports = NewsSync;
